992972ac6e986043d13ee82801afce5f08a01fe3
Maycon Francisco <mayconjf@gmail.com>
2019-12-17 23:08:24 -0300

    add img

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Calculadora IMC implementada em Flutter
 
+<h1 align="center">
+  <img alt="PrintScreen Calculadora IMC" title="Calculadora IMC" src=".github/imc-flutter-img.png" />
+</h1>
--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
       if (imc < 18.6) {
         _infoText = "Abaixo do peso (IMC $imcStr)";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal ($imcStr)";
+        _infoText = "Peso ideal (IMC $imcStr)";
       } else if (imc >= 24.9 && imc < 29.9) {
         _infoText = "Levemente acima do peso (IMC $imcStr)";
       } else if (imc >= 29.9 && imc < 34.9) {
